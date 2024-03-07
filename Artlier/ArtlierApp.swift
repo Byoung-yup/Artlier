cc61c00302a412bf59e0bcd424b87685b14a963f
ByoungYup <20170926@vision.hoseo.edu>
2024-03-07 16:08:16 +0900

    Initial Commit

---
+++ Artlier/ArtlierApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtlierApp.swift
+//  Artlier
+//
+//  Created by 김병엽 on 2024/03/07.
+//
+
+import SwiftUI
+
+@main
+struct ArtlierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
